17621f36196567291fbd0c4889630d171653bb2c
mariasofia200 <mariarincon1@colegioguanenta.edu.co>
2024-02-22 17:53:38 -0500

    se realizo analisis y diseño

--- operacionesartimeticas.py
+++ operacionesartimeticas.py
@@ -15,3 +15,9 @@ p=x**y
 print("resultado")
 
 print ("la suma de "+str(x) +" + "+ str(y)+"es"+str(s))  
+print("la resta de"+str(x)+ "-" + str(y)+"es"+str(S))
+print("la multiplicacion de"+str(x)+ "*" + str(y)+"es"+str(s))
+print("la division de"+str(x)+ "/" + str(y)+"es"+str(s))
+print("la division entera de"+str(x)+ "//" + str(y)+"es"+str(s))
+print("el modulo de"+str(x)+ "%" + str(y)+"es"+str(s))
+print("la potencia de"+str(x)+ "**" + str(y)+"es"+str(s))
